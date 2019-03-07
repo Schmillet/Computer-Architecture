6ef5cd4497bf014627bde587c9c8640a5463898b
Andrew Kolkmeier <44323778+Schmillet@users.noreply.github.com>
2019-03-06 22:35:04 -0600

    Add files via upload

--- Kolkmeier.s
+++ Kolkmeier.s
@@ -1,10 +1,24 @@
 #Andrew Kolkmeier
-#This code merges two ordered lists
+#This program merges two ordered lists
 	.data
-list1:
-list2:
-combined_list:
+list1:			.word 1, 2, 5, 8
+list1_size: 	.word 4
+list2: 			.word 3, 4, 6, 7
+list2_size:		.word 4
+combined_list:  .word 0 
 
 	.text
 	.globl main
-main:
+main:	
+		#initialization
+		la $t0, list1 #stores address of list 1 $t0
+		lw $s0, list1_size #stores size of list 1 $s0
+		la $t1, list2 #stores address of list 2 in $t1
+		lw $s1, list2_size #stores size of list 2 $s1
+		add $s2, $s0, $s1 #stores the size of combined list in $s2
+
+
+
+
+
+
