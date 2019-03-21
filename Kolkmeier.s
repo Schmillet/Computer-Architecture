45029d9b4a7ff6c30f7539e5b3a88243cd94bf68
Andrew Kolkmeier <44323778+Schmillet@users.noreply.github.com>
2019-03-20 19:20:25 -0500

    Better merge algorithm

--- Kolkmeier.s
+++ Kolkmeier.s
@@ -5,10 +5,10 @@
 #must be manually entered as well
 
 	.data
-newLine: 		.asciiz " "
 list1:			.word 1, 2, 5, 8
 list2: 			.word 3, 4, 6, 7
-combined_list:  .word 0, 0, 0, 0, 0, 0, 0, 0
+combined_list:  .word 0
+newLine: 		.asciiz " "
 
 
 	.text
@@ -24,11 +24,15 @@ main:	#initialization
 
 		move $s6, $zero 			#s6 is the list 1 iteration variable
 		move $s7, $zero 			#s7 is the list 2 iteration variable	
+
+		sll $t3, $s1, 2				#length of list1 times four
+		sll $t4, $s3, 2				#length of list2 times four
+		sll $t5, $s5, 2				#length of combined_list times four
 		
  #main merge loop
 loop_1:
-		bgt $s6, 13, exit_1 		#while loop conditions
-		bgt $s7, 13, exit_1
+		beq $s6, $t3, exit_1 		#while loop conditions
+		beq $s7, $t4, exit_1
 
 		lw $t0, 0($s0)				#address pointer to list 1
 		
@@ -57,7 +61,7 @@ First_if_exit:
 exit_1:
 
 loop_2:
-		beq $s6, 16, exit_2			#Exit loop if list1 is empty
+		beq $s6, $t3, exit_2			#Exit loop if list1 is empty
 
 		sw $t0, 0($s4)				#stores remaining value into combined_list
 
@@ -72,7 +76,7 @@ exit_2:
 
 
 loop_3:
-		beq $s7, 16, exit_3			#Exit loop if list2 is empty
+		beq $s7, $t4, exit_3			#Exit loop if list2 is empty
 
 		sw $t1, 0($s4)				#stores remaining value into combined_list
 
@@ -84,11 +88,11 @@ loop_3:
 		j loop_3
 
 exit_3:
-		sub $s4, $s4, 32			#Sets the address for combined_list back to the front
+		sub $s4, $s4, $t5     		#Sets the address for combined_list back to the front
 
 		addi $t9, $zero, 0			#$t9 is the index for the while loop 
 print:
-		beq $t9, 32, print_exit
+		beq $t9, $t5, print_exit
 
 		lw $t7, 0($s4) 				#Address pointer to combined_list
 
